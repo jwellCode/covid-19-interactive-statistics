da9ee037b6bdaeac9b7cee59b04341727a19e4b3
zi-mt <j.herrchen@gmx.de>
2021-01-05 11:11:13 +0100

    temperature and humidity toggle

--- components/D3Linechart.vue
+++ components/D3Linechart.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="linechart-container">
-      <svg id="linechart" class="mx-auto"></svg>
-  </div>
+    <div class="linechart-container">
+        <div class="linechart-graph">
+            <svg id="linechart" class="mx-auto"></svg>
+        </div>
+        <div class="linechart-settings"> 
+            <input v-on:input="toggle()" type="checkbox" id="ws" class="weather-switch" name="weather-switch"/>
+            <span class="toggle-label"> Luftfeuchtigkeit </span>
+            <label for="ws" id="weather-toggle" class="toggle">
+                <span class="toggle-knop"></span>
+            </label>
+            <span class="toggle-label"> Temperatur </span>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -30,105 +40,160 @@ export default {
                 margin: 50,
                 width: 500, 
                 height: 500,
-                toppadding: 10
-            }
+                toppadding: 10,
+                colors: {
+                    'temp': 'lightcoral',
+                    'humidity': 'cornflowerblue'
+                }
+            },
+            weatherActive: 'humidity'
+        }
+    },
+    methods: {
+        toggle: function () {
+
+            let svg = d3.select('svg#linechart');
+
+            // Remove previous chart elements
+            d3.select('path.weather-line').remove();
+            d3.select('g.weather-axis').remove();
+            d3.select('g.weather-axis').remove();
+            d3.select('text.weather-label').remove();
+
+            let toggleLabel = document.getElementById('weather-toggle');
+            toggleLabel.classList.contains('toggled') ? toggleLabel.classList.remove('toggled') : toggleLabel.classList.add('toggled');
+
+            this.weatherActive = this.weatherActive === 'temp' ? 'humidity' : 'temp';
+
+            let yScale = d3.scaleLinear()
+                .range([this.chart_config.height, 0])
+                .domain([0, d3.max(this.data, d => d[this.weatherActive]  + this.chart_config.toppadding)]);
+
+            let xScale = d3.scaleTime()
+                .range([0, this.chart_config.width])
+                .domain(d3.extent(this.data, d => d.date ));
+
+            let line = d3.line()
+                .x(d => xScale(d.date))
+                .y(d => yScale(d[this.weatherActive]))
+                .curve(d3.curveCatmullRom.alpha(0.5));
+
+            let yAxis = d3.axisRight(yScale)
+                .ticks(10);
+            
+            // Set Graph
+            svg.append('path')
+                .datum(this.data)
+                .attr('class', `weather-line line-${this.weatherActive}`)
+                .attr('fill', 'none')
+                .attr('stroke', this.chart_config.colors[this.weatherActive])
+                .attr('stroke-width', 3)            
+                .attr('transform', `translate( ${this.chart_config.margin}, ${this.chart_config.margin})`)
+                .attr('d', line);
+
+            // Set y Axis
+            svg.append('g')
+                .attr('class', 'weather-axis y-axis')
+                .attr('transform', `translate( ${this.chart_config.margin + this.chart_config.width}, ${this.chart_config.margin})`)
+                .call(yAxis);
+
+            let label = this.weatherActive === 'temp' ? 'Temperatur (°C)' : 'Luftfeuchtigkeit (%)';
+
+            // Set y Axis Label
+            svg.append('text')
+                .attr('x', this.chart_config.margin )
+                .attr('y',  this.chart_config.margin / 4 )
+                .attr('class', 'weather-label')
+                .attr('transform-origin', 'center center')
+                .attr('transform', 'rotate(90)')
+                .text(label);
         }
     },
     mounted () {
 
         let svg = d3.select('svg#linechart')
             .attr('width', (this.chart_config.height + 2 * this.chart_config.margin))
-            .attr('height', (this.chart_config.height + 2 * this.chart_config.margin))
-            .attr('transform', `translate( ${-this.chart_config.margin}, ${-this.chart_config.margin})`);
+            .attr('height', (this.chart_config.height + 2 * this.chart_config.margin));
 
         let xScale = d3.scaleTime()
             .range([0, this.chart_config.width])
             .domain(d3.extent(this.data, d => d.date ));
-
-        let yScaleTemp = d3.scaleLinear()
-            .range([this.chart_config.height, 0])
-            .domain([0, d3.max(this.data, d => d.temp  + this.chart_config.toppadding)]);
-
-        let yScaleHumidity = d3.scaleLinear()
-            .range([this.chart_config.height, 0])
-            .domain([0, d3.max(this.data, d => d.humidity  + this.chart_config.toppadding)]);
-
-        let lineTemp = d3.line()
-            .x(d => xScale(d.date))
-            .y(d => yScaleTemp(d.temp))
-            .curve(d3.curveCatmullRom.alpha(0.5));
-
-        let lineHumidity = d3.line()
-            .x(d => xScale(d.date))
-            .y(d => yScaleHumidity(d.humidity))
-            .curve(d3.curveCatmullRom.alpha(0.5));
         
         let xAxis = d3.axisBottom(xScale)
             .tickFormat(d3.timeFormat('%b'));
-        let yAxisTemp = d3.axisLeft(yScaleTemp)
-            .ticks(10);
-        let yAxisHumidity = d3.axisRight(yScaleHumidity)
-            .ticks(10);
 
-        // Temperature Graph
-        svg.append('path')
-            .datum(this.data)
-            .attr('class', 'line')
-            .attr('fill', 'none')
-            .attr('stroke', 'lightcoral')
-            .attr('stroke-width', 3)            
-            .attr('transform', `translate( ${this.chart_config.margin}, ${this.chart_config.margin})`)
-            .attr('d', lineTemp);
+        // placeholder for cases axis
+        let yAxis = d3.axisLeft(xScale)
+            .ticks(0);
 
-        // Humidity Graph
-        svg.append('path')
-            .datum(this.data)
-            .attr('class', 'line')
-            .attr('fill', 'none')
-            .attr('stroke', 'cornflowerblue')
-            .attr('stroke-width', 3)            
-            .attr('transform', `translate( ${this.chart_config.margin}, ${this.chart_config.margin})`)
-            .attr('d', lineHumidity);
+        // Weather Graph and Axis
+        this.toggle();
 
         // x Axis (Time)
         svg.append('g')
             .attr('class', 'axis x-axis')
             .attr('transform', `translate( ${this.chart_config.margin}, ${this.chart_config.height + this.chart_config.margin})`)
             .call(xAxis);
-        
-        // y Axis (Temperature)
+
         svg.append('g')
-            .attr('class', 'axis y-axis')
+            .attr('class', 'case-axis y-axis')
             .attr('transform', `translate( ${this.chart_config.margin}, ${this.chart_config.margin})`)
-            .call(yAxisTemp);
-
-        // y Axis Label (Temperature)
-        svg.append("text")
-            .attr("x", this.chart_config.height - this.chart_config.margin )
-            .attr("y",  this.chart_config.margin / 2 )
-            .attr('transform-origin', 'center center')
-            .attr('transform', 'rotate(-90)')
-            .text('Temperatur (°C)');
-        
-        // y Axis (Humidity)
-        svg.append('g')
-            .attr('class', 'axis y-axis')
-            .attr('transform', `translate( ${this.chart_config.margin + this.chart_config.width}, ${this.chart_config.margin})`)
-            .call(yAxisHumidity);
-
-        // y Axis Label (Humidity)
-        svg.append("text")
-            .attr("x", this.chart_config.margin )
-            .attr("y",  this.chart_config.margin / 2 )
-            .attr('transform-origin', 'center center')
-            .attr('transform', 'rotate(90)')
-            .text('Luftfeuchte (%)');
+            .call(yAxis);
     }
 }
 </script>
 
 <style scoped>
     text {
-        @apply text-xl
+        @apply text-xl;
+    }
+
+    input[type=checkbox] {
+        @apply hidden;
+    }
+
+    .linechart-settings {
+        @apply min-w-full;
+        @apply px-64;
+        @apply inline-flex;
+        @apply justify-evenly;
+        @apply items-center;
+        @apply place-self-center;
+    }
+
+    .toggle {
+        @apply block;
+        @apply w-20;
+        @apply h-10;
+        @apply mx-auto;
+        @apply bg-indigo-400;
+        @apply border-4;
+        @apply rounded-full;
+        @apply cursor-pointer;
+    }
+
+    .toggle-knop {
+        @apply block;
+        @apply w-6;
+        @apply h-6;
+        @apply bg-white;
+        @apply rounded-full;
+        @apply cursor-pointer;
+        @apply relative;
+        @apply left-1;
+        @apply top-1;
+
+        @apply transition-transform;
+        @apply duration-150;   
+        @apply ease-out;     
+    }
+
+    .toggled {
+        @apply bg-red-500;
+    }
+
+    .toggled > span.toggle-knop {
+        @apply transform; 
+        @apply translate-x-10;
     }
 </style>
