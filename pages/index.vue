00314d3329b758ab9eb992f8abf611bd9dad6107
jwell <123345x@googlemail.com>
2021-01-25 15:10:31 +0100

    update banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <main id="main" class="w-full max-w-screen-lg mx-auto px-4 sm:px-6 md:px-8">
+  <main
+    id="main"
+    class="w-full max-w-screen-lg mx-auto px-4 sm:px-6 md:px-8 pt-16"
+  >
     <Intro class="mb-24 md:mb-32" />
 
     <ExistingPaper class="mb-24 md:mb-32" />
-    
+
     <LineChart class="mb-24 md:mb-32" />
   </main>
 </template>
