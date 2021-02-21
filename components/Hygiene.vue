114731786fe5fb301a7f68d34001623a8e172ab5
Jason Theiler <turret.jason@outlook.com>
2021-02-21 17:32:35 +0100

    Format all files

--- components/Hygiene.vue
+++ components/Hygiene.vue
@@ -6,41 +6,69 @@
     >
       Dran denken!
     </h2>
+
     <div class="hygiene-container grid grid-cols-2 gap-2 mx-8">
-        <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
-            <img class="w-100 h-3/4 mx-auto mb-2" :src="require(`~/assets/svgs/hygiene-2.svg`)"/>  
-            <div class="text-sm md:text-lg mx-auto text-center">Abstand halten</div>  
-        </div>        
-        <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
-            <img class="w-100 h-3/4 mx-auto mb-2" :src="require(`~/assets/svgs/hygiene-1.svg`)"/>
-            <div class="text-sm md:text-lg mx-auto text-center">Hande gründlich waschen</div> 
+      <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
+        <img
+          class="w-100 h-3/4 mx-auto mb-2"
+          :src="require(`~/assets/svgs/hygiene-2.svg`)"
+        />
+
+        <div class="text-sm md:text-lg mx-auto text-center">Abstand halten</div>
+      </div>
+
+      <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
+        <img
+          class="w-100 h-3/4 mx-auto mb-2"
+          :src="require(`~/assets/svgs/hygiene-1.svg`)"
+        />
+
+        <div class="text-sm md:text-lg mx-auto text-center">
+          Hande gründlich waschen
         </div>
-        <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
-            <img class="w-100 h-3/4 mx-auto mb-2" :src="require(`~/assets/svgs/hygiene-3.svg`)"/>
-            <div class="text-sm md:text-lg mx-auto text-center">Mund-Nasen-Schutz tragen</div> 
+      </div>
+
+      <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
+        <img
+          class="w-100 h-3/4 mx-auto mb-2"
+          :src="require(`~/assets/svgs/hygiene-3.svg`)"
+        />
+
+        <div class="text-sm md:text-lg mx-auto text-center">
+          Mund-Nasen-Schutz tragen
         </div>
-        <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
-            <img class="w-100 h-3/4 mx-auto mb-2" :src="require(`~/assets/svgs/hygiene-5.svg`)"/>
-            <div class="text-sm md:text-lg mx-auto text-center">Zuhause bleiben</div> 
+      </div>
+
+      <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
+        <img
+          class="w-100 h-3/4 mx-auto mb-2"
+          :src="require(`~/assets/svgs/hygiene-5.svg`)"
+        />
+
+        <div class="text-sm md:text-lg mx-auto text-center">
+          Zuhause bleiben
         </div>
-        <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
-            <img class="w-100 h-3/4 mx-auto mb-2" :src="require(`~/assets/svgs/hygiene-4.svg`)"/>
-            <div class="text-sm md:text-lg mx-auto text-center">Desinfizieren</div> 
+      </div>
+
+      <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
+        <img
+          class="w-100 h-3/4 mx-auto mb-2"
+          :src="require(`~/assets/svgs/hygiene-4.svg`)"
+        />
+
+        <div class="text-sm md:text-lg mx-auto text-center">Desinfizieren</div>
+      </div>
+
+      <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
+        <img
+          class="w-100 h-3/4 mx-auto mb-2"
+          :src="require(`~/assets/svgs/hygiene-6.svg`)"
+        />
+
+        <div class="text-sm md:text-lg mx-auto text-center">
+          vielleicht impfen
         </div>
-        <div class="h-28 md:h-48 w-28 md:w-48 my-4 place-self-center">
-            <img class="w-100 h-3/4 mx-auto mb-2" :src="require(`~/assets/svgs/hygiene-6.svg`)"/>
-            <div class="text-sm md:text-lg mx-auto text-center">vielleicht impfen</div> 
-        </div>
+      </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
